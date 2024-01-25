0860ae7ceb937712bd52bb7822c77a52f10d88c5
jb868 <jb67868@ubt-uni.net>
2024-01-25 16:06:25 +0100

    dashnboard pt.1

---
+++ Views/dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/dashboard.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <ul>
+                <li>
+                    <a href="">Ekipet</a>
+                </li>
+                <li>
+                    <a href="">Lojtarët</a>
+                </li>
+                <li>
+                    <a href="">Profili</a>
+                </li>
+            </ul>
+        </div>
+        <div class="main">
+            <div class="profile">
+                <p>Name: </p>
+                <p>Username: </p>
+                <p>Email: </p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
